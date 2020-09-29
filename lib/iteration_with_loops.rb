624c2800a4fcae3db881c8a06764607c4a7e329e
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-29 01:58:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,9 @@
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  lowest_temps = []
+  src.each do |first_array|
+    first_array.sort!
+    lowest_temps << first_array[0]
+  end 
 end
