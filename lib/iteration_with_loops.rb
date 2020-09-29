ebe330fbe63d3f26883eef037afac5c92faf12c3
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-29 02:06:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,8 @@ def find_min_in_nested_arrays(src)
   while count < src.length do
    inner_count = 0 
    while inner_count < src[count].length do 
-     src[count].sort!
-     puts src[count][0]
+     src[count].sort
+     p src[count][0]
      inner_count += 1
    end 
    count += 1 
